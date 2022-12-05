8338c95d4bd234192fe5931f060ecd0b4a88cf95
yanorei32 <yanorei@hotmail.co.jp>
2022-12-06 03:03:14 +0900

    Add default tmp path

--- src/config.rs
+++ src/config.rs
@@ -9,7 +9,13 @@ pub struct Config {
     pub voicevox_host: String,
     pub discord_token: String,
     pub state_path: String,
+
+    #[serde(default = "default_tmp_path")]
     pub tmp_path: String,
+}
+
+fn default_tmp_path() -> String {
+    "/tmp/".to_string()
 }
 
 pub fn init() -> Result<()> {
