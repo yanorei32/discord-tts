09d317d36ee80dc5517185c7ce55dc02c9078ca3
Kisaragi Marine <kisaragi.effective@gmail.com>
2024-06-22 16:00:05 +0900

    extract to const

--- src/filter.rs
+++ src/filter.rs
@@ -60,8 +60,9 @@ where
 fn append_image_attachment_notification(body: &str, image_count: usize) -> Cow<'_, str> {
     if image_count > 0 {
         const IMAGE_WITH_COMMAS: &str = "画像、";
+        const SENT_ON_BLANK: &str = "画像が送信されました";
         // 一応pre-allocate
-        let mut ret = String::with_capacity(body.len() + 1 + (IMAGE_WITH_COMMAS.len()) * (image_count - 1));
+        let mut ret = String::with_capacity(body.len() + 1 + (IMAGE_WITH_COMMAS.len()) * (image_count - 1) + SENT_ON_BLANK.len());
         ret.push_str(body);
         if !body.is_empty() {
             ret.push(' ');
@@ -72,7 +73,7 @@ fn append_image_attachment_notification(body: &str, image_count: usize) -> Cow<'
         }
 
         if body.is_empty() {
-            ret.push_str("画像が送信されました");
+            ret.push_str(SENT_ON_BLANK);
         } else {
             ret.push_str("画像添付");
         }
